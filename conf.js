53e8583015f8ed2349dbd948fca1618a843b74ad
rossroma <yangkai890612@126.com>
2017-02-03 23:54:57 +0800

    update conf.js

--- conf.js
+++ conf.js
@@ -3,9 +3,9 @@
 // 七牛云存储配置信息
 exports.qiniu = {
   // Access Key
-  accessKey: 'kfqZfOlTwhys5WVJxQo4SkRey0IFSsNgYRzaoLEt',
+  accessKey: '',
   // Secret Key
-  secretKey: 'a_bOV7yWMKb9ooJj9MARRIhR5WlnLPkVU-NXk-AN',
+  secretKey: '-NXk-AN',
   // 要上传的空间
   bucket: 'ross',
   // 域名
@@ -16,7 +16,7 @@ exports.qiniu = {
 exports.bmob = {
   'content-type': 'application/json',
   // REST API Key
-  'x-bmob-rest-api-key': '8a3d04621f341624f41a5db35d688abd',
+  'x-bmob-rest-api-key': '',
   // Application ID
-  'x-bmob-application-id': '897d8343de907640340fa6b06684b181'
+  'x-bmob-application-id': ''
 }
